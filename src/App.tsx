c4418506a298f4bcf12ccdd8cdd90592f2711f8d
Tom Jardine-McNamara <tom@itsravenous.com>
2025-09-24 22:59:13 +0100

    clean up demo app for carousel demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
 import "./App.css";
 import { CssScrollSnapCarousel, JsScrollCarousel } from "./components/Carousel";
 import { DinoCard } from "./components/DinoCard";
+import yogaraptorIcon from "./assets/yogaraptor-icon.png";
+import chillodactylIcon from "./assets/chillodactyl-icon.png";
+import tricalmotopsIcon from "./assets/tricalmotops-icon.png";
+
+const Slide = ({ children }) => (
+  <div
+    style={{
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      gap: "0.5rem",
+    }}
+  >
+    {children}
+  </div>
+);
 
 function App() {
   return (
@@ -9,50 +26,45 @@ function App() {
         height: "100vh",
       }}
     >
-      <h1>Cards</h1>
+      {/* <h1>Cards</h1>
       <DinoCard
         name="Yogaraptor"
         image="https://example.com/yogaraptor.png"
         description="A small, agile dinosaur."
-      />
+        onRelease={() => alert("Yogaraptor released!")}
+      /> */}
 
-      <h1>Carousels</h1>
+      <h1>Dino Carousels</h1>
+      <h2>Scroll snap via JS</h2>
       <JsScrollCarousel>
-        <div>Slide 1</div>
-        <div>Slide 2</div>
-        <div>Slide 3</div>
-        <div>Slide 4</div>
+        <Slide>
+          <img alt="" src={yogaraptorIcon} />
+          Yogaraptor
+        </Slide>
+        <Slide>
+          <img alt="" src={chillodactylIcon} />
+          Chillodactyl
+        </Slide>
+        <Slide>
+          <img alt="" src={tricalmotopsIcon} />
+          Tricalmatops
+        </Slide>
       </JsScrollCarousel>
 
+      <h2>Scroll snap via CSS</h2>
       <CssScrollSnapCarousel>
-        <div>
-          <DinoCard
-            name="Yogaraptor"
-            image="https://example.com/yogaraptor.png"
-            description="A small, agile dinosaur."
-          />
-        </div>
-        <div>
-          <DinoCard
-            name="Tyrannosaurus Rex"
-            image="https://example.com/trex.png"
-            description="A large carnivorous dinosaur."
-          />
-        </div>
-        <div>
-          <DinoCard
-            name="Triceratops"
-            image="https://example.com/triceratops.png"
-            description="A herbivorous dinosaur with three horns."
-          />
-        </div>
-        <div>
-          <DinoCard
-            name="Velociraptor"
-            image="https://example.com/velociraptor.png"
-            description="A small, fast dinosaur."
-          />
-        </div>
+        <Slide>
+          <img alt="" src={yogaraptorIcon} />
+          Yogaraptor
+        </Slide>
+        <Slide>
+          <img alt="" src={chillodactylIcon} />
+          Chillodactyl
+        </Slide>
+        <Slide>
+          <img alt="" src={tricalmotopsIcon} />
+          Tricalmatops
+        </Slide>
       </CssScrollSnapCarousel>
     </div>
   );
